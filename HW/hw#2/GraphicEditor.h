01a1e8f45b0ea84e2b27dcee2a43b42f20c04728
커비 <48703334+MainUps@users.noreply.github.com>
2023-11-20 11:09:15 +0900

    Update GraphicEditor.h

--- HW/hw#2/GraphicEditor.h
+++ HW/hw#2/GraphicEditor.h
@@ -2,11 +2,11 @@
 #include<iostream>
 using namespace std;
 
-class GraphicEditor { // 파생클래스
-    int index; // 도형의 인덱스
+class GraphicEditor { 
+    int index; 
 public:
     GraphicEditor(int i) { index = i; }
-    virtual void show() = 0; // 각 도형들의 출력함수를 구현하기 위해 순수 가상 함수 사용
+    virtual void show() = 0; 
     int getIndex() { return index; }
     void subIndex() { --index; }
 };
